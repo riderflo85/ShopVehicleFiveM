18823af858e8f4a5d92c3302084a3e611109e3c5
Florent <grenaille.florent85@gmail.com>
2020-02-12 20:05:43 +0100

    Updated models.py

--- graphical_menu/models.py
+++ graphical_menu/models.py
@@ -3,8 +3,10 @@ from django.db import models
 
 class Vehicle(models.Model):
     name = models.CharField(max_length=150)
+    model = models.CharField(max_length=150)
     category = models.CharField(max_length=150)
     price = models.IntegerField(null=False)
+    imglink = models.TextField()
 
 class Category(models.Model):
     name = models.CharField(max_length=150)
