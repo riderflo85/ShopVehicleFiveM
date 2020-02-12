d8ac4c3085a4dbaeacd5d02c65b4819a8ee4a8dc
riderflo85 <grenaille.florent85@gmail.com>
2020-02-12 12:56:14 +0100

    create models for database

--- graphical_menu/models.py
+++ graphical_menu/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Vehicle(models.Model):
+    name = models.CharField(max_length=150)
+    category = models.CharField(max_length=150)
+    price = models.IntegerField(null=False)
+
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+    label = models.CharField(max_length=150)
+
